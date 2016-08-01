cf2c4cd7a77cfd2ee9729d294750626ef065fac4
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-08-01 20:44:28 +0100

    License stuff

--- src/rust_lsp_server.rs
+++ src/rust_lsp_server.rs
@@ -1,4 +1,12 @@
-// Note: Rust newbie code ahead (-_-)'
+// Copyright 2016 Bruno Medeiros
+//
+// Licensed under the Apache License, Version 2.0 
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0>. 
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+// WARNING: Rust newbie code ahead (-_-)'
 
 
 extern crate serde_json;
