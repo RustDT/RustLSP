a33180b77d642f6da80a45cb7aa66dca50d002d4
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-09-29 15:19:46 +0100

    Made serde_codegen build-dep optional. Move DummyLSP to own crate.

--- build.rs
+++ build.rs
@@ -1,13 +1,21 @@
-extern crate serde_codegen;
+
+fn main() {
+    inner::main();
+}
 
 #[cfg(feature = "gen_sources")]
-pub fn main() {
-	use std::env;
-	use std::path::Path;
+mod inner {
+	extern crate serde_codegen;
 
-    serde_codegen::expand(&Path::new("src/lsp.IN.rs"), &Path::new("src/lsp.rs")).unwrap();
+	pub fn main() {
+		use std::env;
+		use std::path::Path;
+	
+	    serde_codegen::expand(&Path::new("src/lsp.IN.rs"), &Path::new("src/lsp.rs")).unwrap();
+	}
 }
 
 #[cfg(not(feature = "gen_sources"))]
-pub fn main() {
+mod inner {
+    pub fn main() {}
 }
---
+++ lib/DummyLSP/src/lib.rs
@@ -0,0 +1,104 @@
+// Copyright 2016 Bruno Medeiros
+//
+// Licensed under the Apache License, Version 2.0 
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0>. 
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+extern crate rust_lsp;
+extern crate melnorme_json_rpc as json_rpc;
+
+use json_rpc::service_util::*;
+use rust_lsp::lsp::*;
+
+pub struct DummyLanguageServer {
+	
+}
+
+/**
+ * A no-op language server
+ */ 
+impl DummyLanguageServer {
+	
+	// FIXME: user general error
+	pub fn error_not_available<DATA>(data : DATA) -> ServiceError<DATA> {
+		let msg = "Functionality not implemented.".to_string();
+		let err : ServiceError<DATA> = ServiceError { code : 1, message : msg, data : data };
+		err
+	}
+	
+}
+
+impl LanguageServer for DummyLanguageServer {
+	
+	fn initialize(&self, _: InitializeParams) -> LSResult<InitializeResult, InitializeError> {
+		Ok(InitializeResult { capabilities : ServerCapabilities::default() })
+	}
+	fn shutdown(&self, _: ()) -> LSResult<(), ()> {
+		Ok(())
+	}
+	fn exit(&self, _: ()) {
+	}
+	
+	fn showMessage(&self, _: ShowMessageParams) {}
+	fn showMessageRequest(&self, _: ShowMessageRequestParams) {}
+	fn logMessage(&self, _: LogMessageParams) {}
+	fn telemetryEvent(&self, _: any) {}
+	fn workspaceChangeConfiguration(&self, _: DidChangeConfigurationParams) {}
+	fn didOpenTextDocument(&self, _: DidOpenTextDocumentParams) {}
+	fn didChangeTextDocument(&self, _: DidChangeTextDocumentParams) {}
+	fn didCloseTextDocument(&self, _: DidCloseTextDocumentParams) {}
+	fn didSaveTextDocument(&self, _: DidSaveTextDocumentParams) {}
+	fn didChangeWatchedFiles(&self, _: DidChangeWatchedFilesParams) {}
+	
+	fn publishDiagnostics(&self, _: PublishDiagnosticsParams) {}
+
+	fn completion(&self, _: TextDocumentPositionParams) -> LSResult<CompletionList, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn resolveCompletionItem(&self, _: CompletionItem) -> LSResult<CompletionItem, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn hover(&self, _: TextDocumentPositionParams) -> LSResult<Hover, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn signatureHelp(&self, _: TextDocumentPositionParams) -> LSResult<SignatureHelp, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn gotoDefinition(&self, _: TextDocumentPositionParams) -> LSResult<Vec<Location>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn references(&self, _: ReferenceParams) -> LSResult<Vec<Location>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn documentHighlight(&self, _: TextDocumentPositionParams) -> LSResult<DocumentHighlight, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn documentSymbols(&self, _: DocumentSymbolParams) -> LSResult<Vec<SymbolInformation>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn workspaceSymbols(&self, _: WorkspaceSymbolParams) -> LSResult<Vec<SymbolInformation>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn codeAction(&self, _: CodeActionParams) -> LSResult<Vec<Command>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn codeLens(&self, _: CodeLensParams) -> LSResult<Vec<CodeLens>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn codeLensResolve(&self, _: CodeLens) -> LSResult<CodeLens, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn formatting(&self, _: DocumentFormattingParams) -> LSResult<Vec<TextEdit>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn rangeFormatting(&self, _: DocumentRangeFormattingParams) -> LSResult<Vec<TextEdit>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn onTypeFormatting(&self, _: DocumentOnTypeFormattingParams) -> LSResult<Vec<TextEdit>, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+	fn rename(&self, _: RenameParams) -> LSResult<WorkspaceEdit, ()> {
+		Err(DummyLanguageServer::error_not_available(()))
+	}
+}
