bc77df6b657547b6faa697dedfbe6fea5febd8e3
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-09-29 13:44:13 +0100

    Don't run source generation by default.

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
 extern crate serde_codegen;
 
-use std::env;
-use std::path::Path;
-
+#[cfg(feature = "gen_sources")]
 pub fn main() {
+	use std::env;
+	use std::path::Path;
+
     serde_codegen::expand(&Path::new("src/lsp.IN.rs"), &Path::new("src/lsp.rs")).unwrap();
+}
+
+#[cfg(not(feature = "gen_sources"))]
+pub fn main() {
 }
