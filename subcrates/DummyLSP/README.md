52ce29bc4d0f111f09bedab7c3a37830269b8846
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-10-07 19:11:40 +0100

    Add TCP socket connection mode

--- subcrates/DummyLSP/README.md
+++ subcrates/DummyLSP/README.md
@@ -3,4 +3,7 @@ A Language Server Protocol server implementation for a fake/dummy language.
 
 The purpose of this tool is to help test LSP clients. **WORK IN PROGRESS**
 
-DummyLSP is compiled into an executable that communicates with LSP client via stdin/stdout. TODO: TCP sockets
+DummyLSP is compiled into an executable that communicates with LSP client via stdin/stdout or via TCP sockets. 
+Command line:
+ * `dummy_lsp`: communicate via stdin/stdout
+ * `dummy_lsp <port number>`: listen on localhost, on given port number 
--- subcrates/DummyLSP/src/main.rs
+++ subcrates/DummyLSP/src/main.rs
@@ -2,12 +2,62 @@ extern crate dummy_lsp;
 
 
 use dummy_lsp::*;
+use std::env;
+use std::io;
+use std::io::Write;
+use std::rc::Rc;
 
 fn main() {
-	let ls = std::rc::Rc::new(DummyLanguageServer{ });
+	let ls = Rc::new(DummyLanguageServer{ });
+
+	if env::args().len() == 1  {
+		// Use stdin/stdout
+		
+		let stdin = std::io::stdin();
+		let out_provider = move || std::io::stdout();
+		rust_lsp::lsp_server::LSPServer::start_new(ls, &mut stdin.lock(), out_provider);
+	} else {
+		let mut args = env::args();
+		args.next();
+		let port = args.next().unwrap();
+		tcp_server(port);
+	}
 	
-	let stdin = std::io::stdin();
-	let out_provider = move || std::io::stdout();
+}
+
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+
+
+fn tcp_server(port_str: String) {
+	let port : u16 = port_str.parse::<u16>().expect(&format!("Invalid port number: {}", port_str));
+	let listener = TcpListener::bind(("127.0.0.1", port)).unwrap();
+	println!("listening on port: {}", port);
 	
-	rust_lsp::lsp_server::LSPServer::start_new(ls, &mut stdin.lock(), out_provider);
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				thread::spawn(move|| {
+					handle_client(stream)
+				});
+			}
+			Err(err) => {
+				writeln!(&mut io::stderr(), "TCP listen error : {:?}", err).expect("Failed writing to stderr");
+			}
+		}
+	}
+	
+	drop(listener);
+}
+
+fn handle_client(stream: TcpStream) {
+	//FIXME use same server for each connection
+	let ls = Rc::new(DummyLanguageServer{ });
+	
+	let mut input = io::BufReader::new(stream.try_clone().expect("Failed to clone stream"));
+	
+	rust_lsp::lsp_server::LSPServer::start_new(ls, &mut input, || {
+		stream
+	});
 }
