9a8c96a54b73513b7fbc3d1ed988d58cdafc34a3
Matthew Glazar <strager.nds@gmail.com>
2018-09-22 23:58:57 -0700

    Upgrade to languageserver-types 0.9.0 and Serde 1.0
    languageserver-types introduced some breaking changes between version
    0.6.0 and version 0.9.0, and it now requires Serde 1.0. Update RustLSP
    to work with languageserver-types 0.9.0 and Serde 1.0.
    
    This upgrade is a stepping stone to making RustLSP use the latest
    version of languageserver-types.

--- src/server_tests.rs
+++ src/server_tests.rs
@@ -6,7 +6,6 @@ use jsonrpc::method_types::MethodError;
 use jsonrpc::*;
 use ls_types::*;
 
-use jsonrpc::json_util::JsonObject;
 use serde_json::Value;
 
 use std::io;
@@ -40,8 +39,14 @@ pub fn test_run_lsp_server() {
     let init_params = InitializeParams { 
         process_id: None, 
         root_path: None,
+        root_uri: None,
         initialization_options: None,
-        capabilities: Value::Object(JsonObject::new()),
+        capabilities: ClientCapabilities {
+            workspace: None,
+            text_document: None,
+            experimental: None,
+        },
+        trace: TraceOption::Off,
     };
     
     // Create an rpc handle to the server methods
